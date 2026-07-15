3f6b5999e697c8bc2f9de563bf90666d141e9b84
Carlos Eduardo de Melo <154885531+Cmelodev@users.noreply.github.com>
2026-07-15 00:58:18 -0300

    crud 4/4 completo
    PUT e DELETE adicionados

--- cardapio/urls.py
+++ cardapio/urls.py
@@ -5,8 +5,13 @@ from .views import dias_cardapio, listar_cardapios, itens_cardapio, refeicoes, c
 urlpatterns = [
     path("cardapios/", listar_cardapios, name="listar_cardapios"),
     path("dia_cardapio/", dias_cardapio, name="dias_cardapio"),
+    path('dia_cardapio/<int:pk>/', dias_cardapio, name='dias_cardapio'),
     path("semanas_cardapio/", semanas_cardapio, name="semanas_cardapio"),
+    path('semanas_cardapio/<int:pk>/', semanas_cardapio, name='semanas_cardapio'),
     path("refeicoes/", refeicoes, name="refeicoes"),
+    path('refeicoes/<int:pk>/', refeicoes, name='refeicoes'),
     path("categorias_item/", categorias_item, name="categorias_item"),
+    path('categorias_item/<int:pk>/', categorias_item, name='categorias_item'),
     path("itens_cardapio/", itens_cardapio, name="listar_itens_cardapio"),
+    path('itens_cardapio/<int:pk>/', itens_cardapio, name='itens_cardapio'),
 ]   
--- cardapio/views.py
+++ cardapio/views.py
@@ -74,6 +74,29 @@ def dias_cardapio(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        id_dia = request.data.get('id_dia')
+        try:
+            dia_cardapio = DiaCardapio.objects.get(id_dia=id_dia)
+        except DiaCardapio.DoesNotExist:
+            return Response({'error': 'DiaCardapio não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = DiaCardapioSerializer(dia_cardapio, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        id_dia = request.data.get('id_dia')
+        try:
+            dia_cardapio = DiaCardapio.objects.get(id_dia=id_dia)
+        except DiaCardapio.DoesNotExist:
+            return Response({'error': 'DiaCardapio não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+        dia_cardapio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
@@ -89,6 +112,29 @@ def semanas_cardapio(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        id_semana = request.data.get('id_semana')
+        try:
+            semana_cardapio = SemanaCardapio.objects.get(id_semana=id_semana)
+        except SemanaCardapio.DoesNotExist:
+            return Response({'error': 'SemanaCardapio não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = SemanaCardapioSerializer(semana_cardapio, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        id_semana = request.data.get('id_semana')
+        try:
+            semana_cardapio = SemanaCardapio.objects.get(id_semana=id_semana)
+        except SemanaCardapio.DoesNotExist:
+            return Response({'error': 'SemanaCardapio não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+        semana_cardapio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def refeicoes(request):
@@ -103,6 +149,29 @@ def refeicoes(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        id_refeicao = request.data.get('id_refeicao')
+        try:
+            refeicao = Refeicao.objects.get(id_refeicao=id_refeicao)
+        except Refeicao.DoesNotExist:
+            return Response({'error': 'Refeição não encontrada.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = RefeicaoSerializer(refeicao, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        id_refeicao = request.data.get('id_refeicao')
+        try:
+            refeicao = Refeicao.objects.get(id_refeicao=id_refeicao)
+        except Refeicao.DoesNotExist:
+            return Response({'error': 'Refeição não encontrada.'}, status=status.HTTP_404_NOT_FOUND)
+
+        refeicao.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def categorias_item(request):
@@ -117,6 +186,29 @@ def categorias_item(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        id_categoria = request.data.get('id_categoria')
+        try:
+            categoria_item = CategoriaItem.objects.get(id_categoria=id_categoria)
+        except CategoriaItem.DoesNotExist:
+            return Response({'error': 'CategoriaItem não encontrada.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CategoriaItemSerializer(categoria_item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        id_categoria = request.data.get('id_categoria')
+        try:
+            categoria_item = CategoriaItem.objects.get(id_categoria=id_categoria)
+        except CategoriaItem.DoesNotExist:
+            return Response({'error': 'CategoriaItem não encontrada.'}, status=status.HTTP_404_NOT_FOUND)
+
+        categoria_item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def itens_cardapio(request):
@@ -131,3 +223,26 @@ def itens_cardapio(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        id_item = request.data.get('id_item')
+        try:
+            item_cardapio = ItemCardapio.objects.get(id_item=id_item)
+        except ItemCardapio.DoesNotExist:
+            return Response({'error': 'ItemCardapio não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ItemCardapioSerializer(item_cardapio, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        id_item = request.data.get('id_item')
+        try:
+            item_cardapio = ItemCardapio.objects.get(id_item=id_item)
+        except ItemCardapio.DoesNotExist:
+            return Response({'error': 'ItemCardapio não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+        item_cardapio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
